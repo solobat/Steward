63fb099aca5853cb47e14db020bb4b0b9fbf4ef0
杨建川 <yangjianchuan@letv.com>
2017-09-24 16:50:04 +0800

    optimize jenkins plugin

--- js/plugins/jenkins.js
+++ js/plugins/jenkins.js
@@ -9,12 +9,37 @@ define(function (require, exports, module) {
     var util = require('../common/util')
 
     var name = 'jenkins'
-    var key = ['jk', 'jkb', 'jkc', 'jkw']
+    var key = ['jk', 'jkb', 'jkc', 'jkw', 'jkset']
     var icon = chrome.extension.getURL('img/jenkins.png')
     var title = chrome.i18n.getMessage(name + '_title')
     var subtitle = chrome.i18n.getMessage(name + '_subtitle')
     var SERVER_URL = window.localStorage['jenkins_url'] || ''
     var jobs = []
+
+    const keyUrlMap = {
+        'jk': '',
+        'jkb': 'build',
+        'jkc': 'configure',
+        'jkw': 'ws'
+    }
+
+    const actionTips = {
+        seturl: {
+            key: key,
+            icon: icon,
+            id: 'action-seturl',
+            title: '设置jenkins url',
+            desc: '输入jenkins url，回车确认'
+        },
+        errorurl: {
+            key: key,
+            icon: icon,
+            id: 'action-error',
+            title: '获取不到jenkins jobs',
+            desc: '请使用jkset设置正确的jenkins url',
+            isWarn: true
+        }
+    };
 
     function getJobs() {
         if (jobs.length) {
@@ -31,14 +56,14 @@ define(function (require, exports, module) {
                     resolve(jobs)
                 })
                 .catch(() => {
-                    reject('fetch jenkins jobs failed...')
+                    reject([actionTips['errorurl']])
                 })
         })
     }
 
-    function setUrl() {
-        var iptval = window.prompt('jenkins url:')
+    function setUrl(iptval, callback) {
         if (!iptval) {
+            callback('请输入url');
             return
         }
 
@@ -52,22 +77,36 @@ define(function (require, exports, module) {
 
         SERVER_URL = iptval
         window.localStorage['jenkins_url'] = SERVER_URL
+        callback();
     }
 
-    const keyUrlMap = {
-        'jk': '',
-        'jkb': 'build',
-        'jkc': 'configure',
-        'jkw': 'ws'
+    function showActionTips(action) {
+        let actionTip = actionTips[action];
+
+        if (!actionTip) {
+            return;
+        }
+
+        this.showItemList([{
+            key: key,
+            icon: icon,
+            id: actionTip.id,
+            title: actionTip.title,
+            desc: actionTip.desc
+        }]);
     }
 
     function onInput(key) {
-        if (!SERVER_URL) {
-            setUrl()
+        if (this.cmd === 'jkset') {
+            showActionTips.call(this, 'seturl');
+
+            return;
         }
 
         if (!SERVER_URL) {
-            return
+            this.render('jkset ');
+
+            return;
         }
 
         getJobs().then((results) => {
@@ -87,10 +126,30 @@ define(function (require, exports, module) {
                     isWarn: item.healthReport.length && item.healthReport[0].score !== 100
                 }
             }))
-        })
+        }).catch((results) => {
+            this.showItemList(results);
+        });
     }
 
     function onEnter(id) {
+        if (id.startsWith('action-')) {
+            let actionName = id.split('-')[1];
+
+            if (actionName === 'seturl') {
+                setUrl(this.query, (error) => {
+                    if (error) {
+                        alert(error);
+                    } else {
+                        alert('设置成功');
+                        jobs = [];
+                        this.render('jk ');
+                    }
+                });
+            }
+
+            return;
+        }
+
         if (id) {
             window.open(id)
         }
