70c4141e3ba3aa062f3609712ae371adbee578ae
杨建川 <yangjianchuan@letv.com>
2017-10-02 17:25:28 +0800

    add wallpaper save button && wallpaper view

---
+++ js/utils/storage.js
@@ -0,0 +1,35 @@
+define(function(require, exports, module) {
+    const storageFactory = (storgaeArea) => (action) => (key, defaultValue) => {
+        return new Promise((resolve, reject) => {
+            if (action === 'clear') {
+                storgaeArea[action](resp => resolve(resp));
+            } else if (action === 'get') {
+                storgaeArea[action](key, resp => {
+                    if (resp[key]) {
+                        resolve(resp[key]);
+                    } else if (defaultValue) {
+                        resolve(defaultValue);
+                    } else {
+                        resolve(null);
+                    }
+                });
+            } else {
+                storgaeArea[action](key, resp => resolve(resp));
+            }
+        });
+    }
+
+    const actions = ['get', 'set', 'remove', 'clear'];
+    let storage = {};
+
+    ['sync', 'local'].forEach(storageArea => {
+        let factory = storageFactory(chrome.storage[storageArea]);
+
+        let methods = storage[storageArea] = {};
+        actions.forEach(action => {
+            methods[action] = factory(action);
+        });
+    });
+
+    module.exports = storage;
+});
