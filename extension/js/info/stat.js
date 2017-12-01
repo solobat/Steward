a83f753b8dc0f7b628518db6ab299946a9a40e0b
solobat <solopea@gmail.com>
2017-12-01 10:47:43 +0800

    stat update

--- extension/js/info/stat.js
+++ extension/js/info/stat.js
@@ -1,10 +1,17 @@
 
 // Format: (new comments + new stars + new payments) * 20 + (max new users) * 5 >= 500 == one version
 // The pace of development is not affected
+// comment: steward + alfred + jianshu + ssp
 export default {
+    '3.1.4': {
+        star: 35,
+        comment: 15 + 8 + 6 + 14,
+        pay: 3,
+        user: 702,
+        points: (1 + 4) * 20 + (0) * 5
+    },
     '3.1.3': {
         star: 34,
-        // steward + alfred + jianshu + ssp
         comment: 15 + 8 + 3 + 13,
         pay: 3,
         user: 702,
