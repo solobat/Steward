e8bf3adafed07b526db8baffc8ed4bf265054872
solobat <solopea@gmail.com>
2017-12-02 15:55:29 +0800

    version 3.1.6

--- extension/js/info/stat.js
+++ extension/js/info/stat.js
@@ -1,27 +1,20 @@
 
 // Format: (new comments + new stars + new payments) * 20 + (max new users) * 5 >= 500 == one version
 // The pace of development is not affected
-// comment: steward + alfred + jianshu + ssp
+// comment: steward + alfred + ssp
 export default {
+    '3.1.6': {
+        star: 51,
+        comment: 15 + 8 + 35,
+        pay: 3,
+        user: 702,
+        points: (16 + 15) * 20 + (0) * 5
+    },
     '3.1.5': {
         star: 35,
         comment: 15 + 8 + 6 + 14,
         pay: 3,
         user: 702,
         points: (1) * 20 + (0) * 5
-    },
-    '3.1.4': {
-        star: 35,
-        comment: 15 + 8 + 6 + 14,
-        pay: 3,
-        user: 702,
-        points: (1 + 4) * 20 + (0) * 5
-    },
-    '3.1.3': {
-        star: 34,
-        comment: 15 + 8 + 3 + 13,
-        pay: 3,
-        user: 702,
-        points: 530
     }
 }
