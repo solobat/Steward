dc477c9c243ae50aef9ce78e31af4e205166577f
solobat <solopea@gmail.com>
2018-03-11 20:40:04 +0800

    refactor advanced mixin

--- extension/js/helper/index.js
+++ extension/js/helper/index.js
@@ -1,7 +1,60 @@
+/*global EXT_TYPE */
+
 import configHelper from './configHelper'
 import workflowHelper from './workflowHelper'
 import websitesHelper from './websites'
 import wallpaperHelper from './wallpaper'
 import themeHelper from './themeHelper'
 
+const manifest = chrome.runtime.getManifest();
+const version = manifest.version;
+const extType = EXT_TYPE === 'alfred' ? 'Browser Alfred' : 'steward';
+
 export const dataHelpers = [configHelper, workflowHelper, websitesHelper, wallpaperHelper, themeHelper];
+
+function downloadAsJson(exportObj, exportName) {
+    const dataStr = `data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(exportObj))}`;
+    const downloadAnchorNode = document.createElement('a');
+
+    downloadAnchorNode.setAttribute('href', dataStr);
+    downloadAnchorNode.setAttribute('download', `${exportName}.json`);
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+}
+
+export function backup() {
+    const tasks = dataHelpers.map(helper => helper.getData());
+
+    Promise.all(tasks).then(([config, workflows, websites, wallpapers, themes]) => {
+        const data = {
+            config,
+            workflows,
+            websites,
+            wallpapers,
+            themes,
+            meta: {
+                version,
+                export_at: Number(new Date()),
+                app: extType
+            }
+        };
+
+        console.log(data);
+        downloadAsJson(data, 'Steward-Backup');
+    });
+}
+
+export function restoreData(data) {
+    console.log(data);
+    const tasks = dataHelpers.map(helper => {
+        const obj = data[helper.key];
+
+        if (obj) {
+            return helper.setData(obj);
+        } else {
+            return Promise.resolve(null);
+        }
+    });
+
+    return Promise.all(tasks);
+}
--- extension/pages/options/mixins/advanced.js
+++ extension/pages/options/mixins/advanced.js
@@ -1,20 +1,5 @@
-/*global EXT_TYPE */
 import { restoreConfig } from '../../../js/common/config'
-import { dataHelpers } from '../../../js/helper'
-
-const manifest = chrome.runtime.getManifest();
-const version = manifest.version;
-const extType = EXT_TYPE === 'alfred' ? 'Browser Alfred' : 'steward';
-
-function downloadAsJson(exportObj, exportName) {
-    const dataStr = `data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(exportObj))}`;
-    const downloadAnchorNode = document.createElement('a');
-
-    downloadAnchorNode.setAttribute('href', dataStr);
-    downloadAnchorNode.setAttribute('download', `${exportName}.json`);
-    downloadAnchorNode.click();
-    downloadAnchorNode.remove();
-}
+import { backup, restoreData } from '../../../js/helper'
 
 function readFile(file) {
     return new Promise((resolve, reject) => {
@@ -29,50 +14,13 @@ function readFile(file) {
         };
 
         reader.readAsText(file);
-    });
-}
-
-function restoreData(data) {
-    console.log(data);
-    const tasks = dataHelpers.map(helper => {
-        const obj = data[helper.key];
-
-        if (obj) {
-            return helper.setData(obj);
-        } else {
-            return Promise.resolve(null);
-        }
-    });
-
-    return Promise.all(tasks);
+    })
 }
 
 export default {
     methods: {
-        backup() {
-            const tasks = dataHelpers.map(helper => helper.getData());
-
-            Promise.all(tasks).then(([config, workflows, websites, wallpapers, themes]) => {
-                const data = {
-                    config,
-                    workflows,
-                    websites,
-                    wallpapers,
-                    themes,
-                    meta: {
-                        version,
-                        export_at: Number(new Date()),
-                        app: extType
-                    }
-                };
-
-                console.log(data);
-                downloadAsJson(data, 'Steward-Backup');
-            });
-        },
-
         handleExportClick() {
-            this.backup();
+            backup();
         },
 
         handleBackupBeforeUpload(file) {
