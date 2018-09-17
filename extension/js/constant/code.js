868239ed11bd34b62f97c050d3d52361446c8657
tomasy <solopea@gmail.com>
2018-09-17 18:21:29 +0800

    update default code

--- extension/js/constant/code.js
+++ extension/js/constant/code.js
@@ -1,54 +1,54 @@
 
 export const PLUGIN_DEFAULT = `
-// module.exports = function (steward) {
-const version = 1;
-const id = 'solobat';
-const name = 'yourPlugin';
-const key = 'xxx';
-const type = 'keyword';
-const icon = 'http://static.oksteward.com/icon128.png';
-const title = 'your plugin';
-const subtitle = 'custom your plugin';
-const commands = [{
-    key,
-    type,
-    title,
-    subtitle,
-    icon
-}];
+module.exports = function (steward) {
+    const version = 1;
+    const id = 'solobat';
+    const name = 'yourPlugin';
+    const key = 'xxx';
+    const type = 'keyword';
+    const icon = 'http://static.oksteward.com/icon128.png';
+    const title = 'your plugin';
+    const subtitle = 'custom your plugin';
+    const commands = [{
+        key,
+        type,
+        title,
+        subtitle,
+        icon
+    }];
 
-function onInput(query, command) {
-    // return a promise 
-    return Promise.resolve([
-        {
-            icon,
-            key: 1,
-            title: 'result 1',
-            desc: 'desc 1'
-        },
-        {
-            icon,
-            key: 2,
-            title: 'result 2',
-            desc: 'desc 2'
-        }
-    ]);
+    function onInput(query, command) {
+        // return a promise 
+        return Promise.resolve([
+            {
+                icon,
+                key: 1,
+                title: 'result 1',
+                desc: 'desc 1'
+            },
+            {
+                icon,
+                key: 2,
+                title: 'result 2',
+                desc: 'desc 2'
+            }
+        ]);
+    }
+
+    function onEnter(item, command, query, shiftKey, list) {
+        steward.util.copyToClipboard(item.title, true);
+    }
+
+    return {
+        id,
+        version,
+        name,
+        category: 'other',
+        icon,
+        title,
+        commands,
+        onInput,
+        onEnter
+    };
 }
-
-function onEnter(item, command, query, shiftKey, list) {
-    steward.util.copyToClipboard(item.title, true);
-}
-
-return {
-    id,
-    version,
-    name,
-    category: 'other',
-    icon,
-    title,
-    commands,
-    onInput,
-    onEnter
-};
-// }
 `;
