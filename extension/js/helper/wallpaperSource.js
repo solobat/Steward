24247b315d837d7f4277d96049f5c8cda0944847
tomasy <solopea@gmail.com>
2018-12-01 21:26:20 +0800

    update solid colors

--- extension/js/helper/wallpaperSource.js
+++ extension/js/helper/wallpaperSource.js
@@ -5,6 +5,7 @@ import desktoppr from '../api/desktoppr'
 import nasa from '../api/nasa'
 import picsum from '../api/picsum'
 import pixabay from '../api/pixabay'
+import selection from '../api/selection'
 
 function getRandomOne(list) {
     if (list && list.length) {
@@ -19,26 +20,19 @@ const sourceList = [
     desktoppr,
     nasa,
     picsum,
-    pixabay
+    pixabay,
+    selection
 ];
 
 const SOLID_COLORS = [
-    '#009688',
-    '#00acc1',
-    '#0f6cb2',
-    '#0f6bb3',
-    '#19a5aa',
-    '#3f51b5',
-    '#303f9f',
-    '#423c92',
-    '#424242',
-    '#5e35b1',
-    '#607d8b',
-    '#7e57c2',
-    '#c5cae9',
-    '#ff8f00',
-    '#ff9e80',
-    '#ff6e40'
+    '#f3a696', '#efcd9a', '#005e15', '#b4866b', '#7ebea5', '#a5cd89', '#fcf5f7', '#d8c6bc', '#574738',
+    '#005baa', '#8cb155', '#5ac2d9', '#1e88a8', '#998c78', '#aa8c63', '#624498', '#f58f98', '#478384',
+    '#858954', '#767c6b', '#a58f86', '#726250', '#f4a466', '#727171', '#c92e36', '#6f6f43', '#22825d',
+    '#ada250', '#73b8e2', '#8491c3', '#b28c6e', '#864337', '#513743', '#b36c3c', '#888abc', '#400b36',
+    '#373c38', '#c37854', '#824880', '#465d4c', '#ffd768', '#b48a76', '#595045', '#9ea1a3', '#f0b694',
+    '#d3a243', '#00695b', '#606da1', '#0086cc', '#ee7948', '#00a497', '#405c36', '#7cc28e', '#b56c60',
+    '#7065a3', '#eb9793', '#fcd4d5', '#c5591a', '#efbb2c', '#b0ca71', '#6c2c2f', '#f8eed1', '#71a4d9',
+    '#8c6589', '#595455', '#5b7e91'
 ];
 
 const sourcesInfo = {
