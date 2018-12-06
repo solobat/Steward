1060e5cedd03e7fbe9d7a14b4b0df043fc67612b
tomasy <solopea@gmail.com>
2018-12-07 00:08:05 +0800

    update todo action

--- extension/js/helper/actionHelper.js
+++ extension/js/helper/actionHelper.js
@@ -2,6 +2,7 @@ import PageCommand from '../../js/enum/pageCommand'
 
 const defaultActions = [{
     title: 'Toggle TODO',
+    desc: 'Add a bookmark and tag it with a TODO prefix / Remove bookmark',
     actionType: PageCommand.TOGGLE_TODO
 }, {
     title: 'Toggle protection status',
