abb7c1df72a980ed15bc343a144991c3ebff6023
tomasy <solopea@gmail.com>
2019-03-11 21:27:00 +0800

    update download plugin

--- extension/js/plugins/browser/download.js
+++ extension/js/plugins/browser/download.js
@@ -2,7 +2,12 @@
  * @description download
  * @author tomasy
  * @email solopea@gmail.com
+ * @usage enter to open file in folder or pause/resume task
+ *        metaKey + enter to cancel
+ *        shiftKey + enter to open url
  */
+
+import util from '../../common/util'
 
 const chrome = window.chrome;
 const version = 2;
@@ -39,19 +44,30 @@ function searchDownload(query, callback) {
     }, QUERY_DELAY);
 }
 
-const rFilename = /(?!\/)[^\/]+\.?(\w+)?$/;
+const rFilename = /(?!\/)[^/]+\.?(\w+)?$/;
 
-function formatTitle (item) {
-    const size = (item.fileSize / 1024 / 1024).toFixed(2);
+function getItemTitle (item) {
+    const filename = item.filename.match(rFilename)[0];
+    let extraInfo = '';
 
-    return [
-        item.filename.match(rFilename)[0],
-        '   [',
-        `${size}Mb`,
-        ']   ',
-        (new Date(item.endTime)).toLocaleString()
-    ].join('');
+    if (item.state === 'in_progress') {
+        const pausedInfo = item.paused ? 'paused' : 'downloading';
+
+        extraInfo = `/ [${(item.bytesReceived / item.totalBytes * 100).toFixed(2)}%] / [${pausedInfo}]`;
+    } else if (item.state === 'interrupted') {
+        extraInfo = '[interrupted]';
+    }
+
+    return `${filename} ${extraInfo}`;
 }
+
+function getItemDesc(item) {
+    const size = (item.fileSize / 1024 / 1024).toFixed(2);
+    const endTime = (new Date(item.endTime)).toLocaleString();
+
+    return `${size}Mb / ${endTime} / ${item.url}`;
+}
+
 function onInput(query) {
     return new Promise(resolve => {
         searchDownload(query, function (downloadList) {
@@ -70,9 +86,12 @@ function onInput(query) {
                     id: item.id,
                     icon: icon,
                     url: item.url,
-                    title: formatTitle(item),
-                    desc: item.url,
-                    isWarn: false
+                    title: getItemTitle(item),
+                    desc: getItemDesc(item),
+                    paused: item.paused,
+                    state: item.state,
+                    filename: item.filename.match(rFilename)[0],
+                    isWarn: item.paused && item.state === 'in_progress'
                 });
             }
 
@@ -81,8 +100,28 @@ function onInput(query) {
     });
 }
 
-function onEnter(item) {
-    chrome.downloads.show(item.id);
+function onEnter(item, command, query, keyStatus) {
+    if (item.state === 'in_progress') {
+        if (keyStatus.metaKey) {
+            chrome.downloads.cancel(item.id);
+            util.toast.info(`[${item.filename}] has been canceled`);
+        } else {
+            if (item.paused) {
+                chrome.downloads.resume(item.id);
+                util.toast.info(`[${item.filename}] has been resumed`);
+            } else {
+                chrome.downloads.pause(item.id);
+                util.toast.info(`[${item.filename}] has been paused`);
+            }
+        }
+        window.stewardApp.refresh();
+    } else {
+        if (keyStatus.shiftKey) {
+            util.createTab(item);
+        } else {
+            chrome.downloads.show(item.id);
+        }
+    }
 }
 
 export default {
